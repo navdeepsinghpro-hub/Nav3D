83cbac8e158a34a89f06fa1da7d2ce8d3b4220ba
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-06-20 21:42:49 +0530

    Phase 8 - user regestration saved to database

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,20 +1,35 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from app.models.schemas import UserRegister
+from app.models.user import User
+from app.database import get_db
 from app.auth.security import hash_password
 
 router = APIRouter()
 
 
 @router.post("/register")
-def register(user: UserRegister):
+def register(
+    user: UserRegister,
+    db: Session = Depends(get_db)
+):
 
     hashed_password = hash_password(
         user.password
     )
 
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=hashed_password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
     return {
-        "username": user.username,
-        "email": user.email,
-        "hashed_password": hashed_password
+        "message": "User registered successfully",
+        "user_id": new_user.id
     }
