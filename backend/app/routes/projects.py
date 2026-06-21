98acb2517adecf99186c5d32e6ca339850afddc5
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-06-21 12:52:48 +0530

    Phase 16 - Save projects to SQLite

--- backend/app/routes/projects.py
+++ backend/app/routes/projects.py
@@ -1,12 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app.models.project import Project
 from app.schemas.project import ProjectCreate
 
 router = APIRouter()
 
 
 @router.post("/projects")
-def create_project(project: ProjectCreate):
+def create_project(
+    project: ProjectCreate,
+    db: Session = Depends(get_db)
+):
+    new_project = Project(
+        name=project.name,
+        owner_id=1
+    )
+
+    db.add(new_project)
+    db.commit()
+    db.refresh(new_project)
+
     return {
-        "message": "Project created",
-        "name": project.name
+        "message": "Project saved",
+        "project_id": new_project.id,
+        "name": new_project.name
     }
