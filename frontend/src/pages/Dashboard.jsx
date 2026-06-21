a5cc436ec7ff5577fcc3cafa581c6894557a3c9b
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-06-21 13:21:24 +0530

    Phase 19 - Create projects from dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 function Dashboard() {
   const [user, setUser] = useState(null);
   const [projects, setProjects] = useState([]);
+  const [projectName, setProjectName] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -48,6 +49,39 @@ setProjects(projectsData);
     navigate("/login");
   };
 
+  const createProject = async () => {
+  if (!projectName.trim()) return;
+
+  try {
+    const response = await fetch(
+      "http://127.0.0.1:8000/projects",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: projectName,
+        }),
+      }
+    );
+
+    const newProject = await response.json();
+
+    setProjects([
+      ...projects,
+      {
+        id: newProject.project_id,
+        name: newProject.name,
+      },
+    ]);
+
+    setProjectName("");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
   return (
     <div className="min-h-screen bg-black text-white p-10">
       <h1 className="text-4xl font-bold mb-6">
@@ -67,6 +101,24 @@ setProjects(projectsData);
           <p className="mb-6">
             Email: {user.email}
           </p>
+        <div className="mb-6">
+          <input
+            type="text"
+            placeholder="Project Name"
+            value={projectName}
+            onChange={(e) =>
+              setProjectName(e.target.value)
+            }
+            className="bg-gray-900 p-3 rounded mr-3"
+          />
+
+          <button
+            onClick={createProject}
+            className="bg-white text-black px-4 py-3 rounded"
+          >
+            Create Project
+          </button>
+        </div>
 
           <h3 className="text-xl mt-6 mb-3">
             My Projects
